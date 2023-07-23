f4a583140e5fbe42317f907a7f6b6ffb208054b5
DogLooksGood <doglooksgood@hotmail.com>
2023-07-24 02:26:57 +0800

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Solana integration for Race Protocol";
+
+  inputs = {
+    nixpkgs = { url = "github:NixOS/nixpkgs/nixos-23.05"; };
+    flake-utils = { url = "github:numtide/flake-utils"; };
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+        {
+          devShell = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              rustc
+              cargo
+              solana-cli
+            ];
+          };
+        }
+    );
+}
