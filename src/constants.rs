e2b35c1c5b606e229265368998b57725b2eb6ed2
DogLooksGood <doglooksgood@gmail.com>
2026-01-29 00:09:34 +0800

    Bump profile seed

--- src/constants.rs
+++ src/constants.rs
@@ -1,5 +1,5 @@
-pub const PLAYER_PROFILE_SEED: &str = "race-player-1000";
-pub const SERVER_PROFILE_SEED: &str = "race-server-1000";
+pub const PLAYER_PROFILE_SEED: &str = "race-player-2000";
+pub const SERVER_PROFILE_SEED: &str = "race-server-2000";
 pub const SERVER_ACCOUNT_LEN: usize = 108;
 pub const MAX_SERVER_NUM: usize = 10;
 pub const PROFILE_ACCOUNT_LEN: usize = 130;
